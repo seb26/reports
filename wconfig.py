b4a419be24a51b5d1cb46be3fdb768559346b229
seb26 <unleashed26@gmail.com>
2011-09-18 21:56:12 +1000

    re-add sample wconfig

---
+++ wconfig.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2011 seb26. All rights reserved.
+# Source code is licensed under the terms of the Modified BSD License.
+
+config = {
+
+    'tfwiki': {
+        'usr': 'username',
+        'pwd': 'password',
+
+        'url': 'http://wiki.teamfortress.com/w/',
+        'url-api': 'http://wiki.teamfortress.com/w/api.php',
+
+        },
+
+    'pwiki': {
+        'usr': 'username',
+        'pwd': 'password',
+
+        'url': 'http://theportalwiki.com/w/',
+        'url-api': 'http://theportalwiki.com/w/api.php',
+
+        }
+
+}
